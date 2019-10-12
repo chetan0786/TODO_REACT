7fedc41f3ca7f3a4628644112975753ed45163c2
chetan0786 <devganchetan6@gmail.com>
2019-10-12 16:04:11 +0530

    done

---
+++ build/precache-manifest.7778c9be7828bbb41bb5151f0f40e905.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6bb82efad55dd6e0494f19f380745e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0305c32f4d94b944ad2c",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "27bf900f9b5ed0fc6453",
+    "url": "/static/js/2.048796cb.chunk.js"
+  },
+  {
+    "revision": "0305c32f4d94b944ad2c",
+    "url": "/static/js/main.c2d9db94.chunk.js"
+  },
+  {
+    "revision": "f11085f9bfb347f85e77",
+    "url": "/static/js/runtime-main.e8f99f95.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
